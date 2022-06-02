9e7d24c283e63a8eab3ed8fde09501c9bef65b97
excelitai-gunjon <gunjon@excelitai.com>
2022-06-02 17:32:56 +0600

    auto otp fill

---
+++ android/app/src/main/kotlin/com/example/smsotp_autofill/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smsotp_autofill
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:smsotp_autofill/send_otp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SendOTPScreen(),
+    );
+  }
+}
